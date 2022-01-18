fd28ff9a4331006d8a2f95bf4c95ef2b38ec2b53
DevLeopoldo <leopublinet@gmail.com>
2022-01-18 10:21:16 -0300

    Adicionar arquivos de projeto.

---
+++ UdemyCsharp/Carro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCsharp
+{
+    public class Carro
+    {
+        //atributos do Carro
+        public int Portas { get; }
+        public string Modelo { get; }
+        public Cor Cor { get; }
+        private bool ligado = false;
+
+        public bool Ligado // propriedade. define uma maneira de acessar o atributos
+        {
+            get
+            {
+                return ligado;
+            }
+           
+        }
+
+
+        // metodo construtor da classe
+        public Carro(Cor cor, int portas, string modelo)
+        {
+            Cor = cor;
+            Portas = portas;
+            Modelo = modelo;
+            
+        }
+
+
+        // Comportamento do carro
+        public string Ligar()
+        {
+            ligado = true;
+            return "O carro foi ligado.";
+        }
+        public string Desligar()
+        {
+            ligado = false;
+            return "O carro foi desligado.";
+        }
+        public string Andar()
+        {
+            return "O carro esta em movimento.";
+        }
+    }
+}
---
+++ UdemyCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro(Cor.verde, 4, "SLK350"); //Criando Carro
+            //exibir atributos carro
+            Console.WriteLine(" o carro é um " + carro.Modelo + " cor " + carro.Cor + " e tem " + carro.Portas + " portas.");
+            
+            Console.WriteLine(carro.Ligar()); // Ligar carro
+            Console.WriteLine("Ligado? " + carro.Ligado);
+            Console.WriteLine(carro.Andar()); // Andar com o carro
+            Console.WriteLine(carro.Desligar());
+            Console.WriteLine("Ligado? " + carro.Ligado);
+        }
+    }
+}
